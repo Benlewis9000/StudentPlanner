3a2950332b5250a3486b1b224bc13325f215a827
Benlewis9000 <benjaminlewis9000@gmail.com>
2020-03-16 21:58:16 +0000

    Activity accessors.

--- src/java/model/Activity.java
+++ src/java/model/Activity.java
@@ -1,10 +1,46 @@
 package java.model;
 
+import com.sun.javaws.exceptions.InvalidArgumentException;
+
 public class Activity {
 
-    enum ActivityType {
+    private String description;
+    private int hoursTaken;
 
+    /**
+     * Constructor for an Activity.
+     * @param description what the activity consisted of.
+     * @param hoursTaken to complete the activity.
+     * @throws InvalidArgumentException if description contains commas, or hoursTaken is negative.
+     */
+    public Activity(String description, int hoursTaken) throws InvalidArgumentException{
 
+        if (hoursTaken < 0) throw new IllegalArgumentException("Cannot set negative hours for an Activity.");
+
+        if (description.contains(",")) throw new IllegalArgumentException("Text may not include commas.");
+
+        this.description = description;
+        this.hoursTaken = hoursTaken;
+
+    }
+
+    /**
+     * Accessor for description.
+     * @return description.
+     */
+    public String getDescription() { return this.description; }
+
+    /**
+     * Accessor for hoursTaken.
+     * @return the hours taken to complete the Activity.
+     */
+    public int getHoursTaken() { return this.getHoursTaken(); }
+
+    @Override
+    public String toString(){
+
+        // Todo - decide on format or straight CSV?
+        return null;
 
     }
 
