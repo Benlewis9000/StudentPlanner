0f4134d849e284651d8dbd0d487196d5f9ad4855
Elie Harris <jmt18sfu@uea.ac.uk>
2020-03-17 12:21:21 +0000

    Created StudyProfile class and added methods

--- src/java/model/StudyProfile.java
+++ src/java/model/StudyProfile.java
@@ -1,4 +1,27 @@
 package java.model;
 
+import java.util.ArrayList;
+
 public class StudyProfile {
+    private String month;
+    private String year;
+    private ArrayList<Module> modules;
+
+    public StudyProfile(String month, String year){
+        this.month = month;
+        this.year = year;
+        this.modules = new ArrayList();
+    }
+
+    public String getMonth() { return month; }
+
+    public String getYear() { return year; }
+
+    public ArrayList<Module> getModules() { return modules; }
+
+    public void setModules(ArrayList<Module> modules) { this.modules = modules; }
+
+    public void addModule(Module m){ modules.add(m); }
+    public void removeModule(Module m){ modules.remove(m); }
+
 }
