c18e5d99bcb3c152ff22874dbe7490069e7eaae0
Benlewis9000 <benjaminlewis9000@gmail.com>
2020-03-31 23:25:25 +0100

    MainApplication singleton. Save on stop.

--- src/main/java/controller/MainApplication.java
+++ src/main/java/controller/MainApplication.java
@@ -5,10 +5,67 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import model.Database;
+import model.Semester;
 
 import java.io.IOException;
 
 public class MainApplication extends Application {
+
+    // Singleton reference to application
+    private static MainApplication instance;
+
+    private Stage stage;
+
+    /**
+     * Get the single instance for the application.
+     * @return application singleton.
+     */
+    public static MainApplication getApplication(){
+        return instance;
+    }
+
+    public MainApplication(){
+
+        super();
+
+        // Prevent second instance of MainApplication if one already exists
+        if (instance != null) throw new UnsupportedOperationException("Singleton MainApplication constructor called more than once. Blocking.");
+        else instance = this;
+
+    }
+
+    /**
+     * Get Stage being used.
+     * @return stage in use.
+     */
+    public Stage getStage() {
+        return stage;
+    }
+
+    @Override
+    public void start(Stage stage) throws IOException{
+
+        this.stage = stage;
+
+       Parent root = FXMLLoader.load(this.getClass().getResource("/OverviewView.fxml"));
+
+        Scene scene = new Scene(root);
+
+        // Set up scene
+        stage.setScene(scene);
+        stage.setTitle("Study Planner");
+        stage.setResizable(false);
+        stage.show();
+
+    }
+
+    @Override
+    public void stop(){
+
+        Database.getDatabase().saveDatabaseToFile();
+
+    }
 
     public static void main(String[] args) {
 
@@ -16,18 +73,4 @@ public class MainApplication extends Application {
 
     }
 
-    @Override
-    public void start(Stage stage) throws IOException{
-
-       //Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("view/OverviewView.fxml"));
-       Parent root = FXMLLoader.load(this.getClass().getResource("/OverviewView.fxml"));
-
-        Scene scene = new Scene(root);
-
-        stage.setScene(scene);
-        stage.setTitle("Study Planner");
-        stage.setResizable(false);
-        stage.show();
-
-    }
 }
